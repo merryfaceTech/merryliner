84786ac5bd48773c3097aafc74d8c267d7e550fd
Tarik Merrlees <tarik.merrylees@madetech.com>
2022-02-22 12:36:59 +0000

    third test written

--- lib/merryliner.rb
+++ lib/merryliner.rb
@@ -6,6 +6,12 @@ module Merryliner
   class Error < StandardError; end
 
   def self.execute(string)
-    string
+    input = string
+    if input.length
+      part1, part2 = input.slice!(0...10)
+      part1 + "\n" + part2
+    else
+      string
+    end
   end
 end
