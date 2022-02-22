03acbb504fffed880d45a19d2c3fbad0ad155182
Tarik Merrlees <tarik.merrylees@madetech.com>
2022-02-22 12:31:16 +0000

    second test passes

--- spec/merryliner_spec.rb
+++ spec/merryliner_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Merryliner do
   end
 
   it "Merryliner returns a string of \'something\'" do
-    output = Merryliner.execute
+    output = Merryliner.execute("something")
     expect(output).to eq("something")
   end
 
