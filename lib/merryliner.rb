16db5aba719d0c9abf53178c69564123aeacce79
Tarik Merrlees <tarik.merrylees@madetech.com>
2022-02-22 15:34:57 +0000

    WIP

--- lib/merryliner.rb
+++ lib/merryliner.rb
@@ -6,17 +6,28 @@ module Merryliner
   class Error < StandardError; end
 
   def self.execute(string, column_length)
-    if string.length > column_length
-      if string[column_length - 1] == " "
-        string[column_length -1] = "\n"
-        part1, part2 = string.slice!(0...column_length -1), string
-        part1 + part2
-      else
-        part1, part2 = string.slice!(0...column_length), string
-        part1 + "\n" + part2
+    words_array = string.split(" ")
+    character_count = 0
+    current_column = 1
+    columns_array = [[]]
+    output_text = ""
+
+
+    words_array.each do |word|
+      columns_array[current_column - 1].push(word)
+      character_count += word.length + 1
+
+      if character_count >= column_length
+        current_column += 1
+        character_count = 0
+        columns_array.push([])
       end
-    else
-      string
     end
+
+    columns_array.each do |column|
+      column.join(" ")
+    end
+
+    columns_array.join("\n")
   end
 end
