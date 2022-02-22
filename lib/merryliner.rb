e1156498bbc2aafef61549554f44f5d0840f9eb4
Tarik Merrlees <tarik.merrylees@madetech.com>
2022-02-22 12:50:49 +0000

    fourth test written and passes

--- lib/merryliner.rb
+++ lib/merryliner.rb
@@ -7,16 +7,14 @@ module Merryliner
 
   def self.execute(string)
     if string.length > 10
-      if string[10] == " "
-        string[10] = "\n"
+      if string[9] == " "
+        string[9] = "\n"
         part1, part2 = string.slice!(0...9), string
         part1 + part2
       else
         part1, part2 = string.slice!(0...10), string
         part1 + "\n" + part2
       end
-
-
     else
       string
     end
